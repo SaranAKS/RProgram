e7f91a38115b924dd61b463f10881632a51ca035
saranAKS <sarananandhavel@gmail.com>
2020-11-05 08:35:08 +0530

    R Programming - Function to find inverse of matrix

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,34 @@
+
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cache inversed matrix")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
+mat <- matrix(rnorm(4),2,2)
+mat
+matinv <- makeCacheMatrix(mat)
+matinv
+cacheSolve(matinv)
